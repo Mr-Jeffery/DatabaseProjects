5ac364f6320f666eebbb4d853fc242f0ead564ba
Mr-Jeffery <zhengfy2021@mail.sustech.edu.cn>
2024-04-07 17:41:13 +0800

    Update load.sh script to source setenv.sh instead of using sh command

--- project1/Cpp/load.sh
+++ project1/Cpp/load.sh
@@ -1,5 +1,5 @@
 export DATA_PATH=$(cd ../../Data && pwd)
-sh ../setenv.sh
+source ../setenv.sh
 
 # psql --command="DROP TABLE IF EXISTS cards;" postgresql://<user>:<password>@<host>:<port>/<db>
 psql --command="DROP TABLE IF EXISTS cards;" postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME
