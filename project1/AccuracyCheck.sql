64fe82b3307c7c89d98739496e3bcb595566d225
Lucky Chen <12110119@mail.sustech.edu.cn>
2024-04-24 14:39:00 +0800

    --query accuracy check debug

--- project1/AccuracyCheck.sql
+++ project1/AccuracyCheck.sql
@@ -1,7 +1,7 @@
--- -- Step 1: Drop the existing primary key constraint
--- ALTER TABLE stations DROP CONSTRAINT stations_pkey;
+-- Step 1: Drop the existing primary key constraint
+-- ALTER TABLE stations DROP CONSTRAINT stations_pkey CASCADE;
 -- ALTER TABLE stations ADD COLUMN station_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY;
---
+
 -- UPDATE stations
 -- SET id = new_id
 -- FROM (SELECT row_number() OVER (ORDER BY some_column) AS new_id, primary_key_or_unique_column
@@ -10,19 +10,17 @@
 
 
 --Q1: The numbrer of stations, in each district, on each line or in total.
---select district, bus_line_name, count(bus_station_name) as number_of_stations from bus_stations join bus_line_details on bus_station_name=name group by district, bus_line_name
 --Q1.1: The number of stations in each district.
 select district, count(name) as number_of_stations
-from bus_stations
+from stations
 group by district;
---issue: 西丽线？
 --Q1.2: The number of stations on each line.
-select bus_line_name, count(bus_station_name) as number_of_stations
-from bus_line_details
-group by bus_line_name;
+select line_name, count(line_details.station_name) as number_of_stations
+from line_details
+group by line_name;
 --Q1.3: The number of stations in total.
 select count(name) as number_of_stations
-from bus_stations;
+from stations;
 
 --Q2: Number of female passengers and male passengers respectively.
 select gender, count(gender)
@@ -76,13 +74,17 @@ where cards.code = '883706296';
 
 --Q7: Query information about a specific subway station, including Chinese name, English name, number of exits, the district it is located in, and the subway line it belongs to.
 (select chinese_name, station_name, count(name), district, line_name
-      from exit_details
-               natural join
-           (select station_name, district, chinese_name, line_name
-            from stations
-                     join line_details on stations.name = line_details.station_name) as station_line
-      where exit_details.station_name = 'Changlingpi'
-      group by chinese_name, station_name, district, line_name);
+ from exit_details
+          natural join
+      (select station_name, district, chinese_name, line_name
+       from stations
+                join line_details on stations.name = line_details.station_name) as station_line
+ where exit_details.station_name = 'Changlingpi'
+ group by chinese_name, station_name, district, line_name);
 
 --Q8: Query information about a specific subway line, including start time, end time, first opening time, number of stations, and an introduction
-select name, start_time, end_time, first_opening, count(line_details.station_name), intro  from lines join line_details on lines.name=line_details.line_name where name='1号线' group by name, start_time, end_time, first_opening, intro;
+select name, start_time, end_time, first_opening, count(line_details.station_name), intro
+from lines
+         join line_details on lines.name = line_details.line_name
+where name = '1号线'
+group by name, start_time, end_time, first_opening, intro;
